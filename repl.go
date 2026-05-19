8d2021eb114b7281eb93c9770aae050e0777154c
Daniel Oliveira <danmiranda227@gmail.com>
2026-05-19 15:08:12 +0100

    feat: add REPL loop with cleanInput and bufio scanner

--- repl.go
+++ repl.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		input := scanner.Text()
+		words := cleanInput(input)
+
+		if len(words) > 0 {
+			fmt.Println("Your command was:", words[0])
+		}
+	}
+}
 
 // split the input into "words"
 func cleanInput(input string) []string {
