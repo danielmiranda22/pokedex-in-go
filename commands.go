bed4beefa5b89599b6f9da504cb7cbc7d95103bf
Daniel Oliveira <danmiranda227@gmail.com>
2026-05-19 16:52:15 +0100

    refactor: split commands into commands.go

---
+++ commands.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/danielmiranda22/pokedexcli/internal/pokeapi"
+)
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp(cmds map[string]cliCommand) func() error {
+	return func() error {
+		fmt.Println("Welcome to the Pokedex!")
+		fmt.Println("Usage:\n")
+		for _, cmd := range cmds {
+			fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+		}
+		fmt.Println()
+		return nil
+	}
+}
+
+func commandMap(client *pokeapi.PokeAPIClient) func() error {
+	return func() error {
+		areas, err := client.GetLocationAreas()
+		if err != nil {
+			return err
+		}
+		for _, area := range areas {
+			fmt.Println(area)
+		}
+		return nil
+	}
+}
+
+func commandMapBack(client *pokeapi.PokeAPIClient) func() error {
+	return func() error {
+		if client.Previous == nil {
+			fmt.Println("you're on the first page")
+			return nil
+		}
+		areas, err := client.GetPreviousLocationAreas()
+		if err != nil {
+			return err
+		}
+		for _, area := range areas {
+			fmt.Println(area)
+		}
+		return nil
+	}
+}
--- repl.go
+++ repl.go
@@ -13,7 +13,6 @@ type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
-	// no api field — commands get client via closure
 }
 
 func startRepl(client *pokeapi.PokeAPIClient) {
@@ -41,54 +40,6 @@ func startRepl(client *pokeapi.PokeAPIClient) {
 		if err := cmd.callback(); err != nil {
 			fmt.Println("Error:", err)
 		}
-	}
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp(cmds map[string]cliCommand) func() error {
-	return func() error {
-		fmt.Println("Welcome to the Pokedex!")
-		fmt.Println("Usage:\n")
-		for _, cmd := range cmds {
-			fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-		}
-		fmt.Println()
-		return nil
-	}
-}
-
-func commandMap(client *pokeapi.PokeAPIClient) func() error {
-	return func() error {
-		areas, err := client.GetLocationAreas()
-		if err != nil {
-			return err
-		}
-		for _, area := range areas {
-			fmt.Println(area)
-		}
-		return nil
-	}
-}
-
-func commandMapBack(client *pokeapi.PokeAPIClient) func() error {
-	return func() error {
-		if client.Previous == nil {
-			fmt.Println("you're on the first page")
-			return nil
-		}
-		areas, err := client.GetPreviousLocationAreas()
-		if err != nil {
-			return err
-		}
-		for _, area := range areas {
-			fmt.Println(area)
-		}
-		return nil
 	}
 }
 
